8c50099e116f1a4ee9732120cf9dc74faf47dcdb
jgbenine <joaobenine@gmail.com>
2024-04-30 19:49:54 -0300

    feat: banner promo

---
+++ app/components/BannerPromo.tsx
@@ -0,0 +1,19 @@
+import Image, { ImageProps } from 'next/image'
+import React from 'react'
+
+
+export const BannerPromo = (props: ImageProps) => {
+  return (
+    <span>
+    <Image
+      width={0}
+      height={0}
+      sizes="100vw"
+      className="h-auto w-full"
+      quality={100} 
+      {...props}
+    />
+  </span>
+  )
+}
+
