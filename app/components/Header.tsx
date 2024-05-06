0a7ff936dcf2b7aa07a99c4c24b9555848533aa4
jgbenine <joaobenine@gmail.com>
2024-05-05 21:26:30 -0300

    feat: login google

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,12 +1,15 @@
+"use client";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 const Header = () => {
+  const { data } = useSession();
   return (
-    <header className="flex justify-between items-center pb-5 pt-3 py-3">
+    <header className="flex items-center justify-between py-3 pb-5 pt-3">
       <Link href="/">
         <Image
           src="/img/logo.png"
@@ -16,6 +19,32 @@ const Header = () => {
           sizes="100vw"
         />
       </Link>
+      {data?.user?.name ? (
+        <>
+          {data?.user?.name}
+          <Button
+            onClick={() => {
+              signOut();
+            }}
+            size="icon"
+            variant="outline"
+            className="border-nopne bg-transparent"
+          >
+            Sair
+          </Button>
+        </>
+      ) : (
+        <Button
+          onClick={() => {
+            signIn();
+          }}
+          size="icon"
+          variant="outline"
+          className="border-nopne bg-transparent"
+        >
+          logar
+        </Button>
+      )}
       <Button
         size="icon"
         variant="outline"
